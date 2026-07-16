3e6f01b715b6fa9cd70b18d413cd84f0d0f5b18f
bychetansharma <chetanodicee@gmail.com>
2026-07-16 18:36:05 +0530

    Update Contact.tsx

--- src/components/Contact.tsx
+++ src/components/Contact.tsx
@@ -240,7 +240,7 @@ export function Contact() {
 
               <div className="overflow-hidden rounded-sm border border-navy-100">
                 <iframe
-                  src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d448196.5258803423!2d76.7635769allowfullscreen="
+                  src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d448196.5258803423!2d76.76357692715698!3d28.64368473218014!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x390cfd5b347eb62d%3A0x37205b715389640!2sDelhi!5e0!3m2!1sen!2sin!4v1784199602579!5m2!1sen!2sin"
                   width="100%"
                   height="320"
                   style={{ border: 0 }}
